890f2b0b81f2f79e2e70b2741f068a1fe6fa40f7
christin23 <113669798+christin23@users.noreply.github.com>
2022-09-16 13:35:55 +0530

    Add files via upload

---
+++ array sum.py
@@ -0,0 +1,8 @@
+a=[10,14,8,34,23,67,47,22]
+print(a)
+b = a[1:]
+print (' slicing from first number to last ' , b )
+c= a[:3]
+print('slicing from first 3 number' , c)
+d=a[:-3]
+print('slicing last 3 number to ' , d)
